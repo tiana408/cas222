dedf9214e33baaab33444581c20aacab43ece12b
Tiana Le <tianale9@gmail.com>
2018-03-12 08:57:29 -0700

    Add files via upload

--- ace/process.php
+++ ace/process.php
@@ -2,32 +2,27 @@
     
 $servername = "localhost";
 $username = "tianaweb_tiana";
-$password = "itsoundsbad123";
+$password = "ilikethesun123";
 $dbname = "tianaweb_registration";
+$users_name = $_POST['name'];
+$users_email = $_POST['email'];
+$users_phone_number = $_POST['phone_number'];
+$users_event = $_POST['event'];
+$users_contact_name = $_POST['contact_name'];
+$users_contact_number = $_POST['contact_number'];
+$users_tshirt_size = $_POST['tshirt_size'];
+$users_gender = $_POST['gender'];
 
-        $name= $_POST['name'];
-        $email= $_POST['email'];
-        $phone_number= $_POST['phone_number'];
-        $event= $_POST['event'];
-        $contact_name= $_POST['contact_name'];
-        $contact_number= $_POST['contact_number'];
-        $tshirt_size= $_POST['tshirt_size'];
-        $gender= $_POST['gender'];
-
-
+     
 // Create connection
-
-$conn=mysqli_connect("localhost", "tianaweb_tiana", "itsoundsbad123", "tianaweb_registration");
-
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-
-         
+     
      $sql ="
-  INSERT INTO `tianaweb_registration`.`form`  ('name', 'email' , 'phone_number' , 'event' , 'contact_name' , 'contact_number' , 'tshirt_size' , 'gender') VALUES (NULL '$name','$email','$phone_number','$event','$contact_name','$contact_number','$tshirt_size','$gender') ";
+     INSERT INTO form  (name, email, phone_number, event, contact_name, contact_number, tshirt_size, gender) VALUES ('$users_name', '$users_email', '$users_phone_number', '$users_event', '$users_contact_name', '$users_contact_number', '$users_tshirt_size', '$users_gender')";
         
    if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
@@ -38,3 +33,4 @@ if ($conn->connect_error) {
 $conn->close();
 
 ?>
+
